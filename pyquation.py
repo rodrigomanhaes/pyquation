b141120ba35d852e2ef70dd75b680adbaaa4d204
Rodrigo Manhães <rmanhaes@gmail.com>
2011-12-11 01:34:52 -0200

    first degree equation with a and b

--- pyquation.py
+++ pyquation.py
@@ -2,12 +2,17 @@ class Variable(object):
 
     def __init__(self):
         self.mul = 1
+        self.add = 0
     
     def __call__(self, n):
-        return n * self.mul
+        return n * self.mul + self.add
         
     def __rmul__(self, n):
         self.mul = n
+        return self
+        
+    def __add__(self, n):
+        self.add = n
         return self
 
 
--- pyquation_test.py
+++ pyquation_test.py
@@ -2,24 +2,34 @@ import unittest
 from should_dsl import should
 from pyquation import variable as x
 
-class EquationTest(unittest.TestCase):
 
-    def test_first_degree_equation_1(self):
-        '''first degree equation with a = 1 and b = 0'''
+class FirstDegreeEquationTest(unittest.TestCase):
+
+    def test_1(self):
+        '''with a = 1 and b = 0'''
         f = x
         f(1) |should| equal_to(1)
         f(2) |should| equal_to(2)
         f(3) |should| equal_to(3)
         f(4) |should| equal_to(4)
         
-    def test_first_degree_equation_2(self):
-        '''first degree equation with a =/= 1 and b = 0'''
+    def test_2(self):
+        '''with a =/= 1 and b = 0'''
         f = 2*x
         f(1) |should| equal_to(2)
         f(2) |should| equal_to(4)
         f(3) |should| equal_to(6)
         f(4) |should| equal_to(8)
+        
+    def test_3(self):
+        '''with a and b'''
+        f = 3*x + 2
+        f(1) |should| equal_to(5)
+        f(2) |should| equal_to(8)
+        f(3) |should| equal_to(11)
+        f(4) |should| equal_to(14)
     
     
 if __name__ == '__main__':
     unittest.main()
+    
